645eb53073397231e769e7e7974af30eebf3bd24
Ashique A B <ab.ashique10@gmail.com>
2026-03-07 02:59:01 +0530

    axios token refactor

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,22 @@
 import axios from "axios";
+// 1. Create a specific instance (Best Practice)
+// This prevents polluting the global axios object
 
-// const token = localStorage.getItem('token')
-axios.defaults.headers.common = {
-    'Authorization': `Bearer ${localStorage.getItem('token')}`
-}
+// 2. Add a Request Interceptor
+axios.interceptors.request.use(
+    (config) => {
+        // This runs before every request
+        const token = localStorage.getItem('token');
+        
+        if (token) {
+            config.headers['Authorization'] = `Bearer ${token}`;
+        }
+        
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
-export default axios
+export default axios;
